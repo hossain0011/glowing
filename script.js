1de1092c4ebff191ea6bfa3d0f0558c9f47c154a
Hossain Chowdhury <111006379+hossain0011@users.noreply.github.com>
2023-03-08 22:24:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,50 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let mybutton = document.getElementById("upBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+window.addEventListener("scroll", reveal);
+function reveal(){
+      var reveals = document.querySelectorAll(".reveal");
+  for(var i = 0; i < reveals.length; i++){
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add('active');
+    }else{
+      reveals[i].classList.remove('active')
+    }
+  }
+}
